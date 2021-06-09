dab1119814abc1dbf60d4421091c9acc69df2c57
José L. Sanchez Goyes <qqpp0090w@gmail.com>
2021-06-09 14:08:50 +0200

    comentarios

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,27 @@
-var sign = prompt("What's your sign?");
+// var sign = prompt("What's your sign?");
 
-if (sign.toLowerCase() == "scorpio") {
-  alert("Wow! I'm a Scorpio too!");
+
+
+// if (sign.toLowerCase() == "scorpio") {
+//   alert("Wow! I'm a Scorpio too!");
+// }
+
+var num1= 0;
+var num2 = 5;
+
+var result ;
+
+//sumar
+
+//restar 
+
+function sumar ( n1, n2) {
+ 
 }
+
+
+// multiplicar
+
+//dividir 
+result = sumar (-------)
+console.log ( 'la suma de ' + num1 + " y " + num2 + " = " + result );
