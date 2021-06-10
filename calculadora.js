e50aa0dabb1c2fb2c1bd41f364698494609e970e
MiguelAngel <miguel.sanchez.goyes@hotmail.com>
2021-06-10 13:40:50 +0200

    comit calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,43 @@
-var sign = prompt("What's your sign?");
+const num1 = 10;
+const num2 = 's';
+var Resultado = [];
 
-if (sign.toLowerCase() == "scorpio") {
-  alert("Wow! I'm a Scorpio too!");
+function numSuma(num1, num2){
+  if (isNaN(num1) || isNaN(num2)) {
+    return 'No es un numero!';
+  }
+  return  num1 + num2 ;
+    
 }
+var hacerSuma = numSuma(num1, num2);
+console.log(hacerSuma);
+
+function numResta(num1, num2){
+  if (isNaN(num1) || isNaN(num2)) {
+    return 'No es un numero!';
+  }
+  return  num1 - num2 ;
+    
+}
+var hacerResta = numResta(num1, num2);
+console.log(hacerResta);
+
+function numMult(num1, num2){
+  if (isNaN(num1) || isNaN(num2)) {
+    return 'No es un numero!';
+  }
+  return  num1 * num2 ;
+    
+}
+var hacerMult = numMult(num1, num2);
+console.log(hacerMult);
+
+function numDiv(num1, num2){
+  if (isNaN(num1) || isNaN(num2)) {
+    return 'No es un numero!';
+  }
+  return  num1 / num2 ;
+    
+}
+var hacerDiv = numDiv(num1, num2);
+console.log(hacerDiv);
