8b283b910b9eb039385249ff5a4c0f40c31c2853
MiguelAngel <miguel.sanchez.goyes@hotmail.com>
2021-06-16 09:14:04 +0200

    asdasd

--- calculadora.js
+++ calculadora.js
@@ -1,78 +1,74 @@
-const num1 = parseFloat(prompt("Operaciones"));
-const num2 = parseFloat(prompt("Operaciones"));
-var Resultado = [];
-
 function calculatorPro() {
   let newNumber;
   let numberList = [];
 
   do {
-    newNumber = prompt('Enter a number or press cancel to stop');
-    numberList.push(newNumber)
+    newNumber = parseFloat(prompt('Enter a number or press cancel to stop'));
+    if (isNaN(newNumber)){
+      alert('introduzca un valor permitido');
+    }else if(!isNaN(newNumber)){
+      numberList.push(newNumber)
+    }
   } while (newNumber !== null)
 
-  
-}
-
-if(!isNaN(num1) && isNaN(num2)){
-  function numRaiz(num1){
-    return Math.sqrt(num1)
-  }
-  var hacerRaiz = numRaiz(num1);
-  console.log(hacerRaiz);
-  
-} else if(isNaN(num1) && !isNaN(num2)){
-  function numRaiz(num2){
-    return Math.sqrt(num2)
-  }
-  var hacerRaiz = numRaiz(num2);
-  console.log(hacerRaiz);
-
-} else if (isNaN(num1) || isNaN(num2)){
-  
-  console.log('No es un numero');
-
-} else if(!isNaN(num1) || !isNaN(num2)){
+  function numRaiz(x){
+    return Math.sqrt(x);
+  };
   function numSuma(num1, num2){
-    
-    return  num1 + num2 ;
-      
-  }
-  var hacerSuma = numSuma(num1, num2);
-  console.log(hacerSuma);
-  
-  function numResta(num1, num2){
-    
+    return  num1 + num2 ;  
+  };
+  function numResta(num1, num2){  
     return  num1 - num2 ;
-      
+  };
+  function numMult(num1, num2){ 
+    return  num1 * num2 ;  
+  };
+  function numDiv(num1, num2){  
+    return  num1 / num2 ; 
   }
-  var hacerResta = numResta(num1, num2);
-  console.log(hacerResta);
+
+  if(!isNaN(num1) && isNaN(num2)){
   
-  function numMult(num1, num2){
+    var hacerRaiz = numRaiz(num1);
+    console.log(hacerRaiz);
     
-    return  num1 * num2 ;
-      
-  }
-  var hacerMult = numMult(num1, num2);
-  console.log(hacerMult);
+  } else if(isNaN(num1) && !isNaN(num2)){
+    
+    var hacerRaiz = numRaiz(num2);
+    console.log(hacerRaiz);
   
-  function numDiv(num1, num2){
+  } else if (isNaN(num1) || isNaN(num2)){
     
-    return  num1 / num2 ;
-      
+    console.log('No es un numero');
+  
+  } else if(!isNaN(num1) || !isNaN(num2)){
+    
+    var hacerSuma = numSuma(num1, num2);
+    console.log(hacerSuma);
+    
+    var hacerResta = numResta(num1, num2);
+    console.log(hacerResta);
+    
+    var hacerMult = numMult(num1, num2);
+    console.log(hacerMult);
+    
+    var hacerDiv = numDiv(num1, num2);
+    console.log(hacerDiv);
   }
-  var hacerDiv = numDiv(num1, num2);
-  console.log(hacerDiv);
+  
 }
+
+
+
+
 
 prompt("New numbers? y/n")
 
-  Case 'y' => calculatorPro()
+  //Case 'y' => calculatorPro()
 
     //Output => 
     //The result of the sum is resultSum
     //The result of the rest is resultRest
     
     
-  Case 'n' => "Bye!"
+ // Case 'n' => "Bye!"
