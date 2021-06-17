d5114f3185f9fcaba815382082936b211beff719
MiguelAngel <miguel.sanchez.goyes@hotmail.com>
2021-06-17 08:37:00 +0200

    calculadora normal

--- calculadora.js
+++ calculadora.js
@@ -1,101 +1,49 @@
-function calculatorPro() {
-  let newNumber;
-  let numberList = [];
+const num1 = parseFloat(prompt("Operaciones"));
+const num2 = parseFloat(prompt("Operaciones"));
+var Resultado = [];
 
-  function numRaiz(numberList){
-    return Math.sqrt(numberList);
-  };
-  function numSuma(num1, num2){
-    return  num1 + num2 ;  
-  };
-  function numResta(num1, num2){  
-    return  num1 - num2 ;
-  };
-  function numMult(num1, num2){ 
-    return  num1 * num2 ;  
-  };
-  function numDiv(num1, num2){  
-    return  num1 / num2 ; 
-  }
-  debugger;
-  
-  do {
-    newNumber = parseFloat(prompt('Enter a number or press cancel to stop'));
-    if (isNaN(newNumber)){
-      alert('introduzca un valor permitido');
-    }else if(!isNaN(newNumber)){
-      numberList.push(newNumber)
-    }
-  } while (newNumber !== null){
-    
-    if(numberList.length === 1){
-        
-    var hacerRaiz = numRaiz(numberList);
-    console.log(hacerRaiz);
-    
-  } else if(isNaN(num1) && !isNaN(num2)){
-    
-    var hacerRaiz = numRaiz(num2);
-    console.log(hacerRaiz);
-  
-  } else if (isNaN(num1) || isNaN(num2)){
-    
-    console.log('No es un numero');
-  
-  } else if(!isNaN(num1) || !isNaN(num2)){
-    
-    var hacerSuma = numSuma(num1, num2);
-    console.log(hacerSuma);
-    
-    var hacerResta = numResta(num1, num2);
-    console.log(hacerResta);
-    
-    var hacerMult = numMult(num1, num2);
-    console.log(hacerMult);
-    
-    var hacerDiv = numDiv(num1, num2);
-    console.log(hacerDiv);
-  }
-}
-  
+function numRaiz(x){
+  return Math.sqrt(x);
+};
+function numSuma(num1, num2){
+  return  num1 + num2 ;  
+};
+function numResta(num1, num2){  
+  return  num1 - num2 ;
+};
+function numMult(num1, num2){ 
+  return  num1 * num2 ;  
+};
+function numDiv(num1, num2){  
+  return  num1 / num2 ; 
 }
 
-
-
-
-
-const inicioCalculo = prompt("New numbers? y/n") 
-switch (inicioCalculo) {
-  case 'y':
-      const elegirOperacion = prompt("Que operacion desea realizar? raiz/suma/resta/multiplicar/dividir") 
-      switch (elegirOperacion) {
-        case 'raiz':
-             calculatorPro();   
-            break;
-        case 'suma':
-            calculatorPro()    
-         break;
-        case 'resta':
-             calculatorPro();
-            
-            break;
-        case 'multipicar':
-            calculatorPro()    
-        break;
-        case 'dividir':
-            calculatorPro()
-            break;
-        default:
-            alert('Elejir una operacion')
-            break;
-      }
-      
-      break;
-  case 'n':    
+if(!isNaN(num1) && isNaN(num2)){
   
-   alert ("Bye!");
-  break;
-  default:
-      alert('Elejir entre y/n')
-      break;
-    }
+  var hacerRaiz = numRaiz(num1);
+  console.log(hacerRaiz);
+  
+} else if(isNaN(num1) && !isNaN(num2)){
+  
+  var hacerRaiz = numRaiz(num2);
+  console.log(hacerRaiz);
+
+} else if (isNaN(num1) || isNaN(num2)){
+  
+  console.log('No es un numero');
+
+} else if(!isNaN(num1) || !isNaN(num2)){
+  
+  var hacerSuma = numSuma(num1, num2);
+  console.log(hacerSuma);
+  
+  var hacerResta = numResta(num1, num2);
+  console.log(hacerResta);
+  
+  var hacerMult = numMult(num1, num2);
+  console.log(hacerMult);
+  
+  var hacerDiv = numDiv(num1, num2);
+  console.log(hacerDiv);
+}
+
